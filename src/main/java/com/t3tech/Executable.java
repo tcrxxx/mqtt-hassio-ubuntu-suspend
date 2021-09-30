4885a4371e0a449abee89690f4c3ea89626c78bb
Thalles Rodrigues <tcarreiro.rodrigues@gmail.com>
2021-09-29 23:07:01 -0300

    - Limit command only 'systemctl suspend'. - Response devolvido via MQTT.

--- src/main/java/com/t3tech/Executable.java
+++ src/main/java/com/t3tech/Executable.java
@@ -6,11 +6,13 @@ import com.t3tech.mqtt.ClienteMQTT;
 import com.t3tech.mqtt.Fila;
 import com.t3tech.mqtt.Subscriber;
 
-public class main {
+public class Executable {
 
 	private static final int MQTT_TOPIC_X_QOS = 0;
-	private static final String MQTT_TOPIC_X = "teste/teste/";
-	private static final String MQTT_TOPIC_X_SEARCH = MQTT_TOPIC_X + "#";
+	/* MQTT TOPIC COMMAND BEST PRATICE: 		cmd/<application>/<context>/<destination-id>/<req-type> */
+	private static final String MQTT_TOPIC_X = "cmd/hassio-ubuntu-suspend/f8:32:e4:90:23:df/suspend/req"; 
+	private static final String MQTT_TOPIC_X_SEARCH = MQTT_TOPIC_X + "/#";
+	private static final String MQTT_TOPIC_X_RESPONSE = "cmd/hassio-ubuntu-suspend/f8:32:e4:90:23:df/suspend/res";
 	private static final String MQTT_SERVER = "tcp://192.168.1.165:1883";
 	private static final String MQTT_SERVER_USER = "mosquitto_user";
     private static final String MQTT_SERVER_PASS = "mqtt099812";
@@ -27,12 +29,18 @@ public class main {
 	      Thread.sleep(1000);
 //            clienteMQTT.publicar(MQTT_TOPIC_X, mensagem.getBytes(), 0);
 	      if(!Fila.queue.isEmpty()) {
+
 	    	int size = Fila.queue.size();
 	    	String msg = Fila.queue.poll();
+
 	    	System.out.println("Atendendo fila: 1/" + size);
 	    	System.out.println("Mensagem tirada da fila: " + msg);  
 	    	System.out.println("------------------------------\t");
-	    	Commander.command(Commander.brokenMessageCommand(msg),true,true);
+
+	    	if(msg.equals("systemctl suspend")) {
+	    		String output = Commander.command(Commander.brokenMessageCommand("systemctl suspend"),true,true);
+	    		clienteMQTT.publicar(MQTT_TOPIC_X_RESPONSE, output.getBytes(), 0);
+	    	}
 	      }
       }
     }
--- src/main/java/com/t3tech/command/Commander.java
+++ src/main/java/com/t3tech/command/Commander.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class Commander {
 
-	public static void command(String[] strings, boolean output, boolean errorOutput) throws IOException {
+	public static String command(String[] strings, boolean output, boolean errorOutput) throws IOException {
 		Runtime rt = Runtime.getRuntime();
 //		String[] commands = {"system.exe", "-get t"};
 //		String[] commands = {"ls"};
@@ -20,12 +20,15 @@ public class Commander {
 		     InputStreamReader(proc.getErrorStream()));
 
 		String s = null;
+		StringBuffer sb = new StringBuffer();
 		
 		if (output) {
 			// Read the output from the command
 			System.out.println("Command output:\t");
 			while ((s = stdInput.readLine()) != null) {
 			    System.out.println(s);
+			    sb.append(s);
+			    sb.append(System.getProperty("line.separator"));
 			}
 			System.out.println("------------------------------\t");
 		}
@@ -35,9 +38,13 @@ public class Commander {
 			System.out.println("Error output command (if any):\t");
 			while ((s = stdError.readLine()) != null) {
 			    System.out.println(s);
+			    sb.append(s);
+			    sb.append(System.getProperty("line.separator"));
 			}
 			System.out.println("------------------------------\t");
 		}
+		
+		return sb.toString();
 	}
 	
 	public static String[] brokenMessageCommand(String str) {
